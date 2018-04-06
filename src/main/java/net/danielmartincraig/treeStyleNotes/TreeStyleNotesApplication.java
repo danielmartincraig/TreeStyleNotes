c88b0042711610c68c7c911c7f3e9a494c1188d2
danielmartincraig <danielmartincraig@gmail.com>
2018-04-05 20:49:18 -0600

    Initial Commit
    Stood up site and set up travis.ci

---
+++ src/main/java/net/danielmartincraig/treeStyleNotes/TreeStyleNotesApplication.java
@@ -0,0 +1,12 @@
+package net.danielmartincraig.treeStyleNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeStyleNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeStyleNotesApplication.class, args);
+	}
+}
---
+++ src/test/java/net/danielmartincraig/treeStyleNotes/TreeStyleNotesApplicationTests.java
@@ -0,0 +1,16 @@
+package net.danielmartincraig.treeStyleNotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreeStyleNotesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
