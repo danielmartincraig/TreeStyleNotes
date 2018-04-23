34c09b1e5208fe00393e6ab4b75ad8cc6534d0bf
danielmartincraig <danielmartincraig@gmail.com>
2018-04-23 09:58:04 -0600

    added index.html

---
+++ src/main/java/net/danielmartincraig/treeStyleNotes/web/HomePageController.java
@@ -0,0 +1,18 @@
+package net.danielmartincraig.treeStyleNotes.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomePageController {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+
+
+}
